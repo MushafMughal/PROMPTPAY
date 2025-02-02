713513ad0342c0f86abd7d22749b0befa841adce
MushafMughal <mushafmughal12@gmail.com>
2025-02-02 08:55:09 +0500

    application renamed

--- PROMPTPAY/wsgi.py
+++ PROMPTPAY/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PromptPay_Django project.
+WSGI config for PROMPTPAY project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PromptPay_Django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PROMPTPAY.settings')
 
 application = get_wsgi_application()
