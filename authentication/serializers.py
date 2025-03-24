b83e0c906e8cef7496842cb7e87256c697c8850b
mushafmughal <mushafmughal12@gmail.com>
2025-03-24 13:21:15 -0500

    added feild to user db

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -5,7 +5,7 @@ from .models import User  # Import User model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['cnic', 'username', 'email', 'phone_number', 'password']
+        fields = ['name','cnic', 'username', 'email','dob', 'phone_number', 'password']
         extra_kwargs = {'password': {'write_only': True}}  # Hide password in response
 
     def create(self, validated_data):
