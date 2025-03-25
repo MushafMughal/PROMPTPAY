a5fc547de466c694ef3d8c2471249b91d888a6a3
MushafMughal <mushafmughal12@gmail.com>
2025-03-25 19:46:24 +0500

    flush Db

--- authentication/views.py
+++ authentication/views.py
@@ -46,30 +46,6 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token["user_id"] = user.id
         return token
 
-    # def validate(self, attrs):
-    #     username_or_email_or_cnic = attrs.get("username")  # DRF default expects 'username'
-    #     password = attrs.get("password")
-
-    #     user = None
-    #     if "@" in username_or_email_or_cnic:  # Login via email
-    #         user = User.objects.filter(email=username_or_email_or_cnic).first()
-    #     elif username_or_email_or_cnic.isdigit():  # Login via CNIC
-    #         user = User.objects.filter(cnic=username_or_email_or_cnic).first()
-    #     else:  # Login via username
-    #         user = User.objects.filter(username=username_or_email_or_cnic).first()
-
-    #     if not user or not check_password(password, user.password):  # Use check_password for hashing
-    #         raise AuthenticationFailed("Invalid credentials")
-
-    #     if not user.is_active:  # Prevent inactive users from logging in
-    #         raise AuthenticationFailed("User account is inactive.")
-
-    #     data = super().validate({"username": user.username, "password": password})  # Generate tokens
-    #     data["username"] = user.username  # Add username to response
-    #     data["user_cnic"] = user.cnic  # Add CNIC to response
-    #     data["user_id"] = user.id  # Add user ID to response
-    #     return data
-
     def validate(self, attrs):
         username_or_email_or_cnic = attrs.get("username")
         password = attrs.get("password")
