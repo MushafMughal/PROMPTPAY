4c1cd26c67656faf2b62459355b8358dd586cc31
MushafMughal <mushafmughal12@gmail.com>
2025-04-11 02:02:51 +0500

    removed trash apis

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -5,6 +5,4 @@ from .views import *
 urlpatterns = [
     path("router/", RouterAPI.as_view(), name="chat"),
     path("transfer-money/", TransferAPI.as_view(), name="Transfer Money"),
-    path("check-missing/", CheckMissingInfoAPI.as_view(), name="check_missing"),
-    path("update/", UpdateDataAPI.as_view(), name="update_data"),
 ]
