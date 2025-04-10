1f579277620c77bf609572ad2a5f4c63495497e5
MushafMughal <mushafmughal12@gmail.com>
2025-04-11 04:19:11 +0500

    API Endpoint update

--- PROMPTPAY/urls.py
+++ PROMPTPAY/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),  # Add this line if missing
-    path("api/transactionalchatbot/", include("chatbot.urls")),
+    path("api/promptpaychatbot/", include("chatbot.urls")),
     path('api/auth/', include('authentication.urls')),  # Authentication URLs
     path('api/core/', include('core_banking.urls')),  # Core banking URLs
     path('api/faqchatbot/', include('rag.urls')),  # RAG URLs
